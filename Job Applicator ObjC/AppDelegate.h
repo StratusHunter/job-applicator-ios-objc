8c3d8acb5a2f9bd82429702bde360e7793e89243
Terence Baker <terence@bulbstudios.com>
2019-03-25 08:24:20 +0000

    setup empty project

---
+++ Job Applicator ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Job Applicator ObjC
+//
+//  Created by Terence Baker on 25/03/2019.
+//  Copyright © 2019 Bulb Studios Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Job Applicator ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Job Applicator ObjC
+//
+//  Created by Terence Baker on 25/03/2019.
+//  Copyright © 2019 Bulb Studios Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
